35754cfaefeaae77e8ef57521c427e913cf891c3
raul g <raulg@mail.com>
2019-09-19 13:41:21 +0200

    initial commit

--- Libreria.js
+++ Libreria.js
@@ -1,96 +1,183 @@
-db.libros.insert( [
-      {
+db.libros.insert( [
+
+      {
+
         "titulo": "Fundamentos de Sistemas de Bases de Datos",
-	"edicion": 1,
-        "autor": {
-          "apellidos": "ELMASRI,",
-          "nombre": "R.A."
-        },
-        "editorial": "Addison-Wesley",
-        "año": "2007",
-        "precio": 45.25
+	"edicion": 1,
+
+        "autor": {
+
+          "apellidos": "ELMASRI,",
+
+          "nombre": "R.A."
+
+        },
+
+        "editorial": "Addison-Wesley",
+
+        "anyo": "2007",
+
+        "precio": 45.25
+
       },
-      {
+      {
+
         "titulo": "Fundamentos de Sistemas de Bases de Datos",
-	"edicion": 2,
-        "autor": {
-          "apellidos": "ELMASRI,",
-          "nombre": "R.A."
-        },
-        "editorial": "Addison-Wesley",
-        "año": "2011",
-        "precio": 50
-      },
-      {
-        "titulo": "Introducción a los Sistemas de Bases de Datos ",
-	"edicion": 1,
-        "autor": {
-          "apellidos": "DATE",
-          "nombre": "C.J."
-        },
-        "editorial": "Addison-Wesley",
-        "año": "2001",
-        "precio": 60.35
-      },
-      {
-        "titulo": "Bases de Datos",
-	"edicion": 1,
-        "autor": [
-          {
-            "apellidos": "Santos",
-            "nombre": "E."
-          },
-          {
-            "apellidos": "Garcia",
-            "nombre": "A."
-          },
-          {
-            "apellidos": "Alonso",
-            "nombre": "S."
-          },
-          {
-            "apellidos": "Alarcon",
-            "nombre": "P."
-          },
-          {
-            "apellidos": "Garbajosa",
-            "nombre": "J."
-          }
-        ],
-        "editorial": "E.U. Informatica",
-        "año": "1998",
-        "precio": 20.00
+	"edicion": 2,
+
+        "autor": {
+
+          "apellidos": "ELMASRI,",
+
+          "nombre": "R.A."
+
+        },
+
+        "editorial": "Addison-Wesley",
+
+        "anyo": "2011",
+
+        "precio": 50
+
       },
-      {
+
+      {
+
+        "titulo": "Introducciï¿½n a los Sistemas de Bases de Datos ",
+	"edicion": 1,
+
+        "autor": {
+
+          "apellidos": "DATE",
+
+          "nombre": "C.J."
+
+        },
+
+        "editorial": "Addison-Wesley",
+
+        "anyo": "2001",
+
+        "precio": 60.35
+
+      },
+
+      {
+
         "titulo": "Bases de Datos",
-	"edicion": 2,
-        "autor": [
-          {
-            "apellidos": "Alonso",
-            "nombre": "S."
-          },
-          {
-            "apellidos": "Alarcon",
-            "nombre": "P."
-          },
-          {
-            "apellidos": "Bollain",
-            "nombre": "M."
-          }
-        ],
-        "editorial": "ETSISI",
-        "año": "2010",
-        "precio": 25.00
-      },
-      {
-        "titulo": "Introducción a las Bases de Datos",
-	"edicion": 1,
-        "autor": {
-          "apellidos": "Pons",
-          "nombre": "O."
-        },
-        "editorial": "Thomson",
-        "año": "2005",
-        "precio": 100.25
-      }
+	"edicion": 1,
+
+        "autor": [
+
+          {
+
+            "apellidos": "Santos",
+
+            "nombre": "E."
+
+          },
+
+          {
+
+            "apellidos": "Garcia",
+
+            "nombre": "A."
+
+          },
+
+          {
+
+            "apellidos": "Alonso",
+
+            "nombre": "S."
+
+          },
+
+          {
+
+            "apellidos": "Alarcon",
+
+            "nombre": "P."
+
+          },
+
+          {
+
+            "apellidos": "Garbajosa",
+
+            "nombre": "J."
+
+          }
+
+        ],
+
+        "editorial": "E.U. Informatica",
+
+        "anyo": "1998",
+
+        "precio": 20.00
+
+      },
+      {
+
+        "titulo": "Bases de Datos",
+	"edicion": 2,
+
+        "autor": [
+
+          {
+
+            "apellidos": "Alonso",
+
+            "nombre": "S."
+
+          },
+
+          {
+
+            "apellidos": "Alarcon",
+
+            "nombre": "P."
+
+          },
+
+          {
+
+            "apellidos": "Bollain",
+
+            "nombre": "M."
+
+          }
+
+        ],
+
+        "editorial": "ETSISI",
+
+        "anyo": "2010",
+
+        "precio": 25.00
+
+      },
+
+      {
+
+        "titulo": "Introducciï¿½n a las Bases de Datos",
+	"edicion": 1,
+
+        "autor": {
+
+          "apellidos": "Pons",
+
+          "nombre": "O."
+
+        },
+
+        "editorial": "Thomson",
+
+        "anyo": "2005",
+
+        "precio": 100.25
+
+      }
+
     ])
